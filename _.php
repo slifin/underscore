58a913223257ea09d3171ffa68176873364cd289
= <sfyire@gmail.com>
2015-10-04 12:17:38 +0100

    PSR-2 Formatting

--- _.php
+++ _.php
@@ -1,43 +1,51 @@
 <?php
-class _{
-	function curry(callable $fn,...$start){
-		return function (...$args) use ($fn, $start){
-			$apply = array_map(function($v) use(&$args){
-				if (is_a($v,'_'))
-					return array_shift($args);
-				return $v;
-			},$start);
-			return @$fn(...$apply);
-		};
-	}
-	function compose(...$fns){
-		$prev = array_shift($fns);
-		foreach ($fns as $fn){
-			$prev = function(...$args) use ($fn, $prev){
-				return $prev($fn(...$args));
-			};
-		}
-		return $prev;
-	}
-	function filter($fn,$data){
-		foreach($data as $k=>$v){
-			if (!$fn($v,$k,$data))
-				unset($data[$k]);
-		}
-		return $data;
-	}
-	function map($fn,...$data){
-		$first = array_shift($data);
-		return array_map(function($val,$key,...$data) use($fn,$first){
-			return $fn($val,$key,$first,...$data);
-		},$first,array_keys($first),...$data);
-	}
-	function pluck($key,$data){
-		return array_column($data,$key);
-	}
-	function reduce($fn,$data,$init=NULL){
-		return array_reduce(array_keys($data),function($carry,$key) use($fn,$data){
-			return $fn($carry,$data[$key],$key,$data);
-		},$init);
-	}
+class _
+{
+    public function curry(callable $fn, ...$start)
+    {
+        return function (...$args) use ($fn, $start) {
+            return @$fn(...array_map(function ($v) use (&$args) {
+                if (is_a($v, '_')) {
+                    return array_shift($args);
+                }
+                return $v;
+            }, $start));
+        };
+    }
+    public function compose(...$fns)
+    {
+        $prev = array_shift($fns);
+        foreach ($fns as $fn) {
+            $prev = function (...$args) use ($fn, $prev) {
+                return $prev($fn(...$args));
+            };
+        }
+        return $prev;
+    }
+    public function filter($fn, $data)
+    {
+        foreach ($data as $k => $v) {
+            if (!$fn($v, $k, $data)) {
+                unset($data[$k]);
+            }
+        }
+        return $data;
+    }
+    public function map($fn, ...$data)
+    {
+        $first = array_shift($data);
+        return array_map(function ($val, $key, ...$data) use ($fn, $first) {
+            return $fn($val, $key, $first, ...$data);
+        }, $first, array_keys($first), ...$data);
+    }
+    public function pluck($key, $data)
+    {
+        return array_column($data, $key);
+    }
+    public function reduce($fn, $data, $init = null)
+    {
+        return array_reduce(array_keys($data), function ($carry, $key) use ($fn, $data) {
+            return $fn($carry, $data[$key], $key, $data);
+        }, $init);
+    }
 }
