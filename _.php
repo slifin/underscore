2f7cf4a31c61ab1f4a1fc5b49bcd4e24286707a9
= <sfyire@gmail.com>
2015-10-04 12:57:19 +0100

    add callable type hint

--- _.php
+++ _.php
@@ -22,7 +22,7 @@ class _
         }
         return $prev;
     }
-    public function filter($fn, $data)
+    public function filter(callable $fn, $data)
     {
         foreach ($data as $k => $v) {
             if (!$fn($v, $k, $data)) {
@@ -31,14 +31,14 @@ class _
         }
         return $data;
     }
-    public function map($fn, ...$data)
+    public function map(callable $fn, ...$data)
     {
         $first = array_shift($data);
         return array_map(function ($val, $key, ...$data) use ($fn, $first) {
             return $fn($val, $key, $first, ...$data);
         }, $first, array_keys($first), ...$data);
     }
-    public function reduce($fn, $data, $init = null)
+    public function reduce(callable $fn, $data, $init = null)
     {
         return array_reduce(array_keys($data), function ($carry, $key) use ($fn, $data) {
             return $fn($carry, $data[$key], $key, $data);
