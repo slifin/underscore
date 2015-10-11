0d5a96d6d8e2556979b50ee01985c3a0e26cd6cd
= <sfyire@gmail.com>
2015-10-11 19:37:21 +0100

    add DocBlocks

--- _.php
+++ _.php
@@ -1,6 +1,11 @@
 <?php
 class _
 {
+    /**
+     * Compose a series of functions together
+     * @param  functions    $fns a series of functions
+     * @return function        combined functions
+     */
     public function compose(...$fns)
     {
         $prev = array_shift($fns);
@@ -11,6 +16,14 @@ class _
         }
         return $prev;
     }
+    /**
+     * Pre populate function with parameters whilst leaving some to be filled in later
+     * the _ object passed in as a paremeter will denote a parameter offset that can be
+     * filled in later
+     * @param  callable $fn     function to partially apply
+     * @param  mixed    $start     first set of arguments
+     * @return function         partially applied function
+     */
     public function curry(callable $fn, ...$start)
     {
         return function (...$args) use ($fn, $start) {
@@ -22,6 +35,14 @@ class _
             }, $start));
         };
     }
+    /**
+     * Filter data elements by the return of the function provided
+     * return true to keep the element in the array
+     * return false to remove the element in the array
+     * @param  callable $fn   evaluation function
+     * @param  array          $data a list to itterate over
+     * @return array          filtered array
+     */
     public function filter(callable $fn, $data)
     {
         foreach ($data as $k => $v) {
@@ -31,6 +52,13 @@ class _
         }
         return $data;
     }
+    /**
+     * [get description]
+     * @param  [type] $base    [description]
+     * @param  [type] $access  [description]
+     * @param  string $delimit [description]
+     * @return [type]          [description]
+     */
     public function get($base, $access, $delimit = '.')
     {
         return array_reduce(explode($delimit, $access), function ($carry, $val) {
@@ -41,6 +69,13 @@ class _
                 $carry])));
         }, $base);
     }
+    /**
+     * itterate over a list and change the elements in a list
+     * based on the return from the provided function
+     * @param  callable $fn   function that will return a new value for the current element in the given array
+     * @param  array   $data one dimensional array
+     * @return array   transform data
+     */
     public function map(callable $fn, ...$data)
     {
         $first = array_shift($data);
@@ -48,6 +83,15 @@ class _
             return $fn($val, $key, $first, ...$data);
         }, $first, array_keys($first), ...$data);
     }
+    /**
+     * itterate over a list calling the provided function on the value of each call
+     * on every itteration a carrying variable is put into the function which contains 
+     * any previously returned values or $init
+     * @param  callable $fn   user defined function for reducing
+     * @param  array    $data user data to itterate over
+     * @param  mixed    $init first value inserted into $carry
+     * @return mixed    returned carry value
+     */
     public function reduce(callable $fn, $data, $init = null)
     {
         return array_reduce(array_keys($data), function ($carry, $key) use ($fn, $data) {
