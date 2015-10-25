ced6aedb39d9a1df7656543fe667622729e242eb
= <sfyire@gmail.com>
2015-10-25 13:11:15 +0000

    rewrite tests to psr-2 and utilise phpunit

--- tests.php
+++ tests.php
@@ -1,51 +1,43 @@
 <?php
 //TODO rewrite as PHPUnit tests
-class tests{
-	function curry(){
-		$test = function($val1,$val2,$val3,$val4){
-			return implode(' ',[$val1,$val2,$val3,$val4]);
+class tests extends PHPUnit_Framework_TestCase {
+	function test_curry() {
+		$test = function ($val1, $val2, $val3, $val4) {
+			return implode(' ', [$val1, $val2, $val3, $val4]);
 		};
-		$test2 = (new _)->curry($test,'this',(new _),'correct',(new _));
-		return $test2('is','hooray');
+		$test2 = (new _)->curry($test, 'this', (new _), 'correct', (new _));
+		$this->assertEquals('this is correct hooray', $test2('is', 'hooray'));
 		//expected return 'this is correct hooray'
 	}
 
-	function compose(){
+	function test_compose() {
 		$add2 = (new _)->compose(
-			function($val){
+			function ($val) {
 				return ++$val;
-			},function($val){
+			}, function ($val) {
 				return ++$val;
 			});
-		return $add2(2);
+		$this->assertEquals(4, $add2(2));
 		//expected return 4
 	}
 
-	function filter(){
-		return (new _)->filter(function($val,$key,$arr){
+	function test_filter() {
+		$this->assertEquals(['hello' => 'world'], (new _)->filter(function ($val, $key, $arr) {
 			return ($key == 'hello');
-		},['hello'=>'world','world'=>'hello']);
+		}, ['hello' => 'world', 'world' => 'hello']));
 		//expected return ['hello'=>'world']
 	}
 
-	function map(){
-		return (new _)->map(function($val,$key,$arr){
+	function test_map() {
+		$this->assertEquals([0 => 'hello', 1 => 'world'], (new _)->map(function ($val, $key, $arr) {
 			return $key;
-		},['hello'=>1,'world'=>2]);
+		}, ['hello' => 1, 'world' => 2]));
 		//expected return [0=>'hello',1=>'world']
 	}
-	function reduce(){
-		return (new _)->reduce(function($carry,$val,$key,$arr){
+	function test_reduce() {
+		$this->assertEquals(9, (new _)->reduce(function ($carry, $val, $key, $arr) {
 			return $carry + ($val + $key);
-		},[1=>1,2=>1,3=>1],0);
+		}, [1 => 1, 2 => 1, 3 => 1], 0));
 		//expected return 9
 	}
 }
-
-$tests = new Tests;
-// echo $tests->curry();
-// echo $tests->compose();
-// var_dump($tests->filter());
-// var_dump($tests->map());
-// var_dump($tests->pluck());
-// echo $tests->reduce();
