dd9325e8a8627d0272b3ce43b7a15a9f256b86f9
= <sfyire@gmail.com>
2015-10-25 18:08:57 +0000

    added more tests for get()

--- tests.php
+++ tests.php
@@ -1,16 +1,21 @@
 <?php
-//TODO rewrite as PHPUnit tests
-class tests extends PHPUnit_Framework_TestCase {
-	function test_curry() {
+class Complex {
+	static function access() {
+		return ['something' => (object) ['something' => 'found']];
+	}
+	function exploit(callable $func) {
+		return $func();
+	}
+}
+class Tests extends PHPUnit_Framework_TestCase {
+	function testCurry() {
 		$test = function ($val1, $val2, $val3, $val4) {
 			return implode(' ', [$val1, $val2, $val3, $val4]);
 		};
 		$test2 = (new _)->curry($test, 'this', (new _), 'correct', (new _));
 		$this->assertEquals('this is correct hooray', $test2('is', 'hooray'));
-		//expected return 'this is correct hooray'
 	}
-
-	function test_compose() {
+	function testCompose() {
 		$add2 = (new _)->compose(
 			function ($val) {
 				return ++$val;
@@ -18,26 +23,37 @@ class tests extends PHPUnit_Framework_TestCase {
 				return ++$val;
 			});
 		$this->assertEquals(4, $add2(2));
-		//expected return 4
 	}
-
-	function test_filter() {
+	function testFilter() {
 		$this->assertEquals(['hello' => 'world'], (new _)->filter(function ($val, $key, $arr) {
 			return ($key == 'hello');
 		}, ['hello' => 'world', 'world' => 'hello']));
-		//expected return ['hello'=>'world']
 	}
-
-	function test_map() {
+	function testMap() {
 		$this->assertEquals([0 => 'hello', 1 => 'world'], (new _)->map(function ($val, $key, $arr) {
 			return $key;
 		}, ['hello' => 1, 'world' => 2]));
-		//expected return [0=>'hello',1=>'world']
 	}
-	function test_reduce() {
+	function testReduce() {
 		$this->assertEquals(9, (new _)->reduce(function ($carry, $val, $key, $arr) {
 			return $carry + ($val + $key);
 		}, [1 => 1, 2 => 1, 3 => 1], 0));
-		//expected return 9
+	}
+	function testGetArrayRetrieval() {
+		$this->assertEquals(55, (new _)->get(['number' => 55], '["number"]'));
+	}
+	function testGetArrayDefault() {
+		$this->assertEquals('not found', (new _)->get(['missed' => 22], '["something"]', 'not found'));
+	}
+	function testGetComplexAccess() {
+		$obj = new Complex;
+		$this->assertEquals('found', (new _)->get($obj, '::access()["something"]->something'));
+	}
+	function testGetExploit() {
+		//Demostrating possible exploit if second parameter is user input
+		$obj = new Complex;
+		$userInput = '->exploit(call_user_func(function(){ echo "userDefinedCodeHere"; }))';
+		(new _)->get($obj, $userInput);
+		$this->expectOutputString('userDefinedCodeHere');
 	}
 }
